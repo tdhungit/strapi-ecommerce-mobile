c1db591471d11cf6207ed0541495ddecb4713efe
Jacky <tdhungit@gmail.com>
2025-12-11 11:18:14 +0700

    refactor: replace `withOpacity` with `withValues` for setting color alpha in box shadow.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -110,7 +110,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     borderRadius: BorderRadius.circular(12),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
+                        color: Colors.grey.withValues(alpha: 0.1),
                         spreadRadius: 1,
                         blurRadius: 3,
                         offset: const Offset(0, 1),
